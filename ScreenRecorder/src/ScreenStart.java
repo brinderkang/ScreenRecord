d7258f247a5dee03bd877fc90dbf9be3359f5bf6
Brinder Kang <34261915+brinderkang@users.noreply.github.com>
2019-08-26 10:32:59 +0530

    First Update

--- ScreenRecorder/src/ScreenStart.java
+++ ScreenRecorder/src/ScreenStart.java
@@ -1,12 +1,15 @@
+import java.awt.AWTException;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class ScreenStart {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, AWTException, InterruptedException {
 		 JFrame f=new JFrame("Button Example");  
 		    JButton b=new JButton("Start"); 
 		    JButton c=new JButton("Stop");
@@ -14,18 +17,53 @@ public class ScreenStart {
 		    c.setBounds(200,100,95,30); 
 		    f.add(b); 
 		    f.add(c);
-		    f.setSize(400,400);  
+		    f.setSize(300,300);  
 		    f.setLayout(null);  
 		    f.setVisible(true); 
+		    f.setTitle("Recording");
+		    
+		    
+		    
 		    
 		    b.addActionListener(new ActionListener(){
 		    	public void actionPerformed(ActionEvent e)
 		    	{
-		    		
+		    		try {
+						startbutton.startRec();
+						System.out.println("Rec Start");
+						 f.setState(Frame.ICONIFIED);
+//					    Thread.sleep(10000);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (AWTException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 		    	}
 
 				
 		    });
+		    
+		    
+		    
+		    
+		    c.addActionListener(new ActionListener(){
+		    	public void actionPerformed(ActionEvent e)
+		    	{
+		    		try {
+						startbutton.stopRec();
+						f.setVisible(false);
+						System.exit(0);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+		    	}
+
+				
+		    });
+		    
 
 	}
 
--- ScreenRecorder/src/startbutton.java
+++ ScreenRecorder/src/startbutton.java
@@ -9,20 +9,29 @@ import static org.monte.media.VideoFormatKeys.DepthKey;
 import static org.monte.media.VideoFormatKeys.ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE;
 import static org.monte.media.VideoFormatKeys.QualityKey;
 
+import java.awt.AWTException;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsEnvironment;
 import java.io.File;
+import java.io.IOException;
 
 import org.monte.media.Format;
+import org.monte.media.Registry;
 import org.monte.media.FormatKeys.MediaType;
 import org.monte.media.math.Rational;
 import org.monte.screenrecorder.ScreenRecorder;
 
 public class startbutton {
 	
+	
+	 static ScreenRecorder screenRecorder; 
+	 
+	public static void startRec() throws IOException, AWTException
+	{
+	
 	// screenRecording Start
 	 StringBuffer verificationErrors = new StringBuffer();   
-	 ScreenRecorder screenRecorder; 
+	
 	 File file = new File(System.getProperty("user.dir")+"\\src\\movieFolder");
 	// Create an instance of GraphicsConfiguration to get the   Graphics configuration  of the Screen. This is needed for ScreenRecorder class.     
 	 GraphicsConfiguration gc = GraphicsEnvironment         
@@ -46,12 +55,14 @@ screenRecorder = new ScreenRecorder(gc,
 						null,file);
 
 
+screenRecorder.start();
 
+	}
 
-
-
-
-
+public static void stopRec() throws IOException
+{
+	  screenRecorder.stop();
+}
 
 
 }
